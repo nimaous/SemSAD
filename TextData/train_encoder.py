b89f4510f84833fb91ca5feea2fe39d567407a4f
Nima Rafiee <rafiee.nima@gmail.com>
2021-03-03 12:10:02 +0100

    clean_code_issue

--- TextData/train_encoder.py
+++ TextData/train_encoder.py
@@ -26,6 +26,12 @@ from torch import nn, optim
 from torch import nn
 from torch.utils.data import DataLoader
 from text_dataset import TextDataset
+import numpy as np
+try:
+    from torch.utils.tensorboard import SummaryWriter
+    vis = True
+except:
+    vis = False 
 from utils import nce,  count_parameters
 from encoder import  ConvEncoder
 
@@ -35,36 +41,31 @@ import tensorboard as tb
 tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
 """
 
-import numpy as np
-vis = True
-try:
-    from torch.utils.tensorboard import SummaryWriter
-except:
-    vis = False 
+
 
 wt = 0.001
-bert_dev =  [0,1,2,3]
-h_dev= [1,2,3]
+bert_dev =  [0, 1, 2, 3]
+h_dev= [1, 2, 3]
 
 
 def get_bert_output(x):
     with torch.no_grad():
             x_dict = tokenizer.batch_encode_plus(x, truncation=True, 
-                                                  max_length= args.max_length, 
+                                                  max_length=args.max_length, 
                                                   padding='max_length',
                                                   return_tensors="pt").to(f'cuda:{bert_dev[0]}')  
             out = bert(**x_dict)
             hs = out[2] #[13, bs, L, 768]
             hs = hs[-1] + hs[-2] + hs[-3] + hs[-4]
-            tokens_sum = hs[:,1:].sum(dim=1).unsqueeze(1) #[bs, 1 , 768]
-            cls = hs[:,0].unsqueeze(1) #[bs, 1, 768]            
+            tokens_sum = hs[:, 1:].sum(dim=1).unsqueeze(1) #[bs, 1 , 768]
+            cls = hs[:, 0].unsqueeze(1) #[bs, 1, 768]            
             return torch.cat((tokens_sum, cls), dim=1)#[bs,2,768] 
 
 def train(epoch, loader, h_net_opt):
     loader = tqdm(loader)
     h_net.train()
     bert.eval()
-    for i , (x1 , x2 , _ , _, _ , label) in enumerate(loader):
+    for i , (x1, x2, _, _, _, label) in enumerate(loader):
         h_net_opt.zero_grad()
         with torch.no_grad():
             x1_embedd = get_bert_output(x1) 
@@ -77,19 +78,22 @@ def train(epoch, loader, h_net_opt):
         h_net_opt.step()
         loader.set_description(
             (
-                f' Epoch: {epoch + 1};  Iteration:{i};   Loss: {loss.item()}  '         
+                f' Epoch: {epoch + 1};  Iteration:{i};   Loss: {loss.item()}'         
             )
         )
         
-    if vis == True:
+    if vis:
         with torch.no_grad(): 
-            writer.add_scalar("NCE Loss", loss.item(), global_step=epoch, walltime=wt) 
-            writer.add_scalar("Accuracy", accuracy.item(), global_step=epoch, walltime=wt) 
+            writer.add_scalar("NCE Loss", loss.item(), 
+                              global_step=epoch, walltime=wt) 
+            writer.add_scalar("Accuracy", accuracy.item(), 
+                              global_step=epoch, walltime=wt) 
             if epoch % 10 == 0: 
                 f_meta = []
-                for sent , l in zip(x1, label):
+                for sent, l in zip(x1, label):
                     f_meta.append(f"{l} - {sent}")
-                writer.add_embedding(tag="h", mat=h1.cpu().numpy(), metadata=f_meta , global_step=epoch)         
+                writer.add_embedding(tag="h", mat=h1.cpu().numpy(), 
+                                     metadata=f_meta, global_step=epoch)         
                 
     
 if __name__ == "__main__":
@@ -109,25 +113,28 @@ if __name__ == "__main__":
     
 
     args = parser.parse_args()
-    h_net = ConvEncoder(2 , 768 , args.h_dim, h_net=True)    
-    print("h_net size" , count_parameters(h_net))
+    h_net = ConvEncoder(2, 768, args.h_dim, h_net=True)    
+    print("h_net size", count_parameters(h_net))
     h_net = nn.DataParallel(h_net, device_ids=h_dev)
     h_net = h_net.to(f'cuda:{h_dev[0]}')     
     
     
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    bert =  BertModel.from_pretrained('bert-base-uncased', output_hidden_states = True,).to(f'cuda:{bert_dev[0]}')
+    bert =  BertModel.from_pretrained('bert-base-uncased', 
+                                      output_hidden_states = True,).to(f'cuda:{bert_dev[0]}')
     bert = nn.DataParallel(bert, device_ids=bert_dev)
     
-    h_net_opt = optim.Adam(h_net.parameters(), lr=args.h_lr, weight_decay=args.weight_decay)    
+    h_net_opt = optim.Adam(h_net.parameters(), 
+                           lr=args.h_lr, 
+                           weight_decay=args.weight_decay)    
     tp = "tokens_train" if os.path.isfile("tokens_train") else None
     dsp = "tf-idf-dict_train" if os.path.isfile("tf-idf-dict_train") else None
     ds = TextDataset(dataset_name=args.dataset_name, 
-                     sufix_name = "train",
-                     th_sus_prt = args.th_sus_prt,
-                     tf_idf_pr = args.tf_idf_pr,  
-                     tokens_path = tp,   
-                     data_stats_path = dsp,                     
+                     sufix_name="train",
+                     th_sus_prt=args.th_sus_prt,
+                     tf_idf_pr=args.tf_idf_pr,  
+                     tokens_path=tp,   
+                     data_stats_path=dsp,                     
                      sync_aug= args.sync_aug,
                      out_cls=[0])
     loader = DataLoader(ds, batch_size=args.bs, shuffle=True, num_workers=24)
@@ -139,7 +146,7 @@ if __name__ == "__main__":
     if not os.path.isdir('runs'):
         os.mkdir('runs')      
     
-    if vis == True:
+    if vis:
         writer = SummaryWriter(f'runs/{exp_name}') 
         
     for epoch in range(args.epoch):
