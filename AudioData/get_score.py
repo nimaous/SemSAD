1e8cd5e7334031394bf2b8fd9dcb2af797670327
rahilgholami <35683197+rahilgholami@users.noreply.github.com>
2021-03-03 12:21:29 +0100

    Update get_score.py

--- AudioData/get_score.py
+++ AudioData/get_score.py
@@ -20,9 +20,9 @@ import torch.nn.functional as F
 norm = torch.nn.PairwiseDistance(p=2)
 
 def best_indices_(inp1, inp2):    
-    inp1_normalized = (inp1/norm(inp1, torch.zeros_like(inp1)).view(-1,1)).squeeze()
-    inp2_normalized = (inp2/norm(inp2, torch.zeros_like(inp2)).view(-1,1)).squeeze()
-    similarity = (torch.matmul(inp1_normalized, inp2_normalized.transpose(1,0)))
+    inp1_normalized = (inp1/norm(inp1, torch.zeros_like(inp1)).view(-1, 1)).squeeze()
+    inp2_normalized = (inp2/norm(inp2, torch.zeros_like(inp2)).view(-1, 1)).squeeze()
+    similarity = (torch.matmul(inp1_normalized, inp2_normalized.transpose(1, 0)))
     _, best_indices = similarity.topk(1, dim=1) 
     best_indices = best_indices.view(inp1_normalized.size(0))
     return best_indices
@@ -30,10 +30,10 @@ def best_indices_(inp1, inp2):
 def best_match_(z_test, z_train):
     bs, L, C = z_train.size()
     test_repeat = z_test.repeat_interleave(z_train.size(1), dim=0)
-    similar_h = torch.matmul(z_train.reshape(bs*L,C).unsqueeze(1), test_repeat.unsqueeze(2)).reshape(bs,L)
-    inp1_normalized = F.normalize(z_train.reshape(bs*L,C), dim=1, p=2)
+    similar_h = torch.matmul(z_train.reshape(bs*L, C).unsqueeze(1), test_repeat.unsqueeze(2)).reshape(bs, L)
+    inp1_normalized = F.normalize(z_train.reshape(bs*L, C), dim=1, p=2)
     inp2_normalized = F.normalize(test_repeat, dim=1, p=2)
-    similar_h = torch.matmul(inp1_normalized.unsqueeze(1), inp2_normalized.unsqueeze(2)).reshape(bs,L)
+    similar_h = torch.matmul(inp1_normalized.unsqueeze(1), inp2_normalized.unsqueeze(2)).reshape(bs, L)
     best_indx = F.one_hot(similar_h.argmax(dim=1), num_classes = L).float() #[bs, L]
     best_indx = best_indx.unsqueeze(-1) #[bs, L, 1]
     best_match = (z_train.view(bs, L, C) * best_indx).sum(dim=1)  #[bs, L, 1]  
@@ -42,10 +42,10 @@ def best_match_(z_test, z_train):
 def get_features(args, h_net, x):
     with torch.no_grad():        
         t_1 = 50 #choosing one time step to be fixed for all of the sequences
-        z = h_net.module.encoder(x)#[N, hdim, L]
-        z = z.transpose(1,2)#[N, L, hdim] 
-        z_t1 = z[:, t_1, :].squeeze() #[N, hdim]
-        z_mean = torch.mean(z, dim=1)#[N, hdim] taking average over L
+        z = h_net.module.encoder(x) #[bs, hdim, L]
+        z = z.transpose(1, 2) #[bs, L, hdim] 
+        z_t1 = z[:, t_1, :].squeeze() #[bs, hdim]
+        z_mean = torch.mean(z, dim=1) #[bs, hdim] taking average over L
     return z, z_t1, z_mean
 
 def matrix_preparation_train(args, max_size, data_loader, h_net):
@@ -71,3 +71,4 @@ def matrix_preparation(args, max_size, data_loader, h_net):
     label_tensor = torch.cat(label_lst, dim=0) 
     z_tensor = torch.cat(z_lst, dim=0)
     return  z_tensor, label_tensor
+
