013767cc7566e8a6039303578cbfb3b1e92b25a9
Nima Rafiee <rafiee.nima@gmail.com>
2021-03-03 12:20:34 +0100

    clean_code_issue

--- TextData/train_discriminator.py
+++ TextData/train_discriminator.py
@@ -19,7 +19,6 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"]= "2,3,0,1"
 import argparse
 from tqdm import tqdm
-
 from transformers import BertTokenizer,BertModel
 import torch
 import torchtext
@@ -30,25 +29,21 @@ from utils import  count_parameters
 from encoder import  ConvEncoder
 from torch.optim.lr_scheduler import LambdaLR
 import numpy as np
-
+try:
+    from torch.utils.tensorboard import SummaryWriter
+    vis = True
+except:
+    vis = False 
 """  if you get tensorflow related error 
 import tensorflow as tf
 import tensorboard as tb
 tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
 """
-
-vis = True
-try:
-    from torch.utils.tensorboard import SummaryWriter
-except:
-    vis = False 
-    
+ 
 wt = 0.001
 log_sigmoid = nn.LogSigmoid()
-
-
-bert_dev =  [0,1,2,3]
-s_dev= [1,2,3]
+bert_dev =  [0, 1, 2, 3]
+s_dev= [1, 2, 3]
 norm = torch.nn.PairwiseDistance(p=2) 
 
 def avg_loss(args, a_pos, a_neg, GN):                   
@@ -60,23 +55,22 @@ def avg_loss(args, a_pos, a_neg, GN):
 def get_bert_output(x):
     with torch.no_grad():
             x_dict = tokenizer.batch_encode_plus(x, truncation=True, 
-                                                  max_length= args.max_length, 
+                                                  max_length=args.max_length, 
                                                   padding='max_length',
                                                   return_tensors="pt").to(f'cuda:{bert_dev[0]}')  
             out = bert(**x_dict)
             hs = out[2] #[13, bs, L, 768]
             hs = hs[-1] + hs[-2] + hs[-3] + hs[-4]
-            tokens_sum = hs[:,1:].sum(dim=1).unsqueeze(1) #[bs, 1 , 768]
-            cls = hs[:,0].unsqueeze(1) #[bs, 1, 768]            
-            return torch.cat((tokens_sum, cls), dim=1)#[bs,2,768]   
+            tokens_sum = hs[:,1:].sum(dim=1).unsqueeze(1)   #[bs, 1 , 768]
+            cls = hs[:, 0].unsqueeze(1)     #[bs, 1, 768]            
+            return torch.cat((tokens_sum, cls), dim=1)  #[bs,2,768]   
 
 
 def train(epoch, loader, s_net_opt, s_net):
     loader = tqdm(loader)
     s_net.train()
     bert.eval()
-
-    for i , (x1 , x2 , x_rnd, _, _ , label) in enumerate(loader):
+    for i , (x1, x2, x_rnd, _, _, label) in enumerate(loader):
         if args.gamma_neg == -1 :
             GN = np.random.randint(args.min_gamma_neg, args.max_gamma_neg)
         else:
@@ -98,21 +92,24 @@ def train(epoch, loader, s_net_opt, s_net):
         s_net_opt.step()
         loader.set_description(
             (
-                f' Epoch: {epoch + 1};  Iteration:{i};   Loss: {loss.item()}  '         
+                f' Epoch: {epoch + 1};  Iteration:{i};   Loss: {loss.item()}'         
             )
         )
         
-    if vis == True:
+    if vis:
         with torch.no_grad(): 
-            writer.add_histogram('Positive Pairs', a_pos.cpu().numpy(), global_step=epoch, walltime=wt)
-            writer.add_histogram('Negative Pairs', a_neg.cpu().numpy(), global_step=epoch, walltime=wt)           
-            writer.add_scalar("CE Loss", loss.item(), global_step=epoch, walltime=wt)        
-            writer.add_scalar("Min Dim", best_dim.item(), global_step=epoch, walltime=wt)             
+            writer.add_histogram('Positive Pairs', a_pos.cpu().numpy(), 
+                                 global_step=epoch, walltime=wt)
+            writer.add_histogram('Negative Pairs', a_neg.cpu().numpy(), 
+                                 global_step=epoch, walltime=wt)           
+            writer.add_scalar("CE Loss", loss.item(), 
+                               global_step=epoch, walltime=wt)        
+            writer.add_scalar("Min Dim", best_dim.item(), 
+                               global_step=epoch, walltime=wt)             
         
                 
               
-if __name__ == "__main__":
-    
+if __name__ == "__main__":    
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset_name', type=str, default='AG_NEWS')
     parser.add_argument('--bs', type=int, default=128)
@@ -132,49 +129,47 @@ if __name__ == "__main__":
     parser.add_argument('--out_cls', type=int, default=0)   
     parser.add_argument('--sync_aug', type=bool, default=True)   
 
-    
-       
     args = parser.parse_args()   
     s_net = ConvEncoder(4, 768, args.s_dim)
     s_net = nn.DataParallel(s_net, device_ids=s_dev)
     s_net = s_net.to(f'cuda:{s_dev[0]}') 
-    s_net_opt = optim.Adam(s_net.parameters(), lr=args.s_lr, weight_decay=args.weight_decay)  
+    s_net_opt = optim.Adam(s_net.parameters(), 
+                           lr=args.s_lr, weight_decay=args.weight_decay)  
     
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    bert =  BertModel.from_pretrained('bert-base-uncased', output_hidden_states = True,).to(f'cuda:{bert_dev[0]}')
+    bert =  BertModel.from_pretrained('bert-base-uncased', 
+                                      output_hidden_states=True,).to(f'cuda:{bert_dev[0]}')
     bert = nn.DataParallel(bert, device_ids=bert_dev)    
     
     
     
     
-    lambda1 = lambda epoch: 1 if epoch <70 else (0.5 if epoch <100 else 0.01 )
-    scheduler = LambdaLR(s_net_opt, lr_lambda= lambda1)  
+    lambda1 = lambda epoch: 1 if epoch < 70 else (0.5 if epoch < 100 else 0.01 )
+    scheduler = LambdaLR(s_net_opt, lr_lambda=lambda1)  
     
     tp = "tokens_train" if os.path.isfile("tokens_train") else None
     dsp = "tf-idf-dict_train" if os.path.isfile("tf-idf-dict_train") else None
     
     ds = TextDataset(dataset_name=args.dataset_name, 
-                     sufix_name = "train",
-                     th_sus_prt = args.th_sus_prt,
-                     tf_idf_pr = args.tf_idf_pr,   
-                     rnd_th_sus_prt = args.rnd_th_sus_prt,
-                     rnd_tf_idf_pr = args.rnd_tf_idf_pr,                       
-                     tokens_path = tp,   
-                     data_stats_path = dsp,
-                     sync_aug = args.sync_aug,
+                     sufix_name="train",
+                     th_sus_prt=args.th_sus_prt,
+                     tf_idf_pr=args.tf_idf_pr,   
+                     rnd_th_sus_prt=args.rnd_th_sus_prt,
+                     rnd_tf_idf_pr=args.rnd_tf_idf_pr,                       
+                     tokens_path=tp,   
+                     data_stats_path=dsp,
+                     sync_aug=args.sync_aug,
                      out_cls=[0])
     
-    loader = DataLoader(ds, batch_size=args.bs, shuffle=True, num_workers=24)
+    loader = DataLoader(ds, batch_size=args.bs, 
+                        shuffle=True, num_workers=24)
     
     exp_name = f'{args.dataset_name}_S_{args.s_dim}' 
-    
-    
     if not os.path.isdir('checkpoint'):
         os.mkdir('checkpoint') 
     if not os.path.isdir('runs'):
-        os.mkdir('runs')      
-    
-    if vis == True:
+        os.mkdir('runs')          
+    if vis:
         writer = SummaryWriter(f'runs/{exp_name}') 
         
     for epoch in range(args.epoch):
